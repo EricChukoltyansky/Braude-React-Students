b839d0e11d12c7470fcd4567765a037bd31f08a4
EricChukoltyansky <kazohelmet@gmail.com>
2023-03-13 09:07:18 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+https://join.slack.com/t/slack-oar8652/shared_invite/zt-1qzyr1u8j-tG3ESvbibDEnGRWSlX5WFw
+https://code.tutsplus.com/courses/javascript-fundamentals
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/EricChukoltyansky/Braude-React-Students.git
+// git push -u origin main
